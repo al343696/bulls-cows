b62167d9065a35b67ca0ba298a1b0b321b01521c
victor <al343696@uji.es>
2019-02-11 17:41:35 +0100

    first

---
+++ app/src/main/java/al343696/uji/es/bullscows/MainActivity.java
@@ -0,0 +1,13 @@
+package al343696.uji.es.bullscows;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
